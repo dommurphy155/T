d3c8393a33b046da4c36d88cedee06a14e888a13
dommurphy155 <218424478+dommurphy155@users.noreply.github.com>
2025-07-15 22:19:44 +0100

    j

--- instrument_selector.py
+++
@@ -1,9 +0,0 @@
-import random
-
-CURRENCY_PAIRS = [
-    "EUR_USD", "USD_JPY", "GBP_USD", "AUD_USD", "USD_CAD",
-    "NZD_USD", "USD_CHF", "EUR_GBP", "EUR_JPY", "GBP_JPY"
-]
-
-def choose_best_instrument():
-    return random.choice(CURRENCY_PAIRS)
